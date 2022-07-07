3c4c4a1a3c140132e2fd9657fda9b605ea6a51da
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-07-07 19:11:40 -0300

    cards en el dom

--- main.js
+++ main.js
@@ -13,17 +13,57 @@ let productosEnElCarrito =[];
 let acumulador = ``;
 let padreCarrito = document.createElement("span");
 
-for (let i = 0; i < productos.length; i++) {
-    if (productos[i].stock) {
-    acumulador += `<div> 
-    <br>
-    ${productos[i].burger} - $${productos[i].price} <br>
-    <button onclick="agregarAlCarrito(${productos[i].id})">Agregar</button> </div>
-    <br>
-    <button onclick="eliminarDelCarrito(${productos[i].id})">Eliminar</button> </div>`;
-    }
+// for (let i = 0; i < productos.length; i++) {
+//     if (productos[i].stock) {
+//     acumulador += `<div> 
+//     <br>
+//     ${productos[i].burger} - $${productos[i].price} <br>
+//     <button onclick="agregarAlCarrito(${productos[i].id})">Agregar</button> </div>
+//     <br>
+//     <button onclick="eliminarDelCarrito(${productos[i].id})">Eliminar</button> </div>`;
+//     }
 
     
+// };
+
+for (let i = 0; i < productos.length; i++) {
+    if (productos[i].stock) {
+        acumulador += `<div class="container px-4 px-lg-5 mt-5">
+        <div class="row mb-5">
+         <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+            <!-- Product image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">${productos[i].burger}</h5>
+                    <!-- Product reviews-->
+                    <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                    </div>
+                    <!-- Product price-->
+                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                    $${productos[i].price}
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                <div onclick="agregarAlCarrito(${productos[i].id})"><a class="btn btn-outline-dark mt-auto" >Agregar al carrito</a></div>
+            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                <div onclick="eliminarDelCarrito(${productos[i].id})"><a class="btn btn-outline-dark mt-auto" >Eliminar del carrito</a></div>
+            </div>
+        </div>
+        </div>
+        </div>`;
+    }
 }
 
 // Reduce para saber el precio total en el carrito
@@ -77,7 +117,7 @@ function eliminarDelCarrito(idDeProducto){
 
 
 document.body.append(padreCarrito);
-document.write(acumulador);
+document.getElementById("cardProduct").innerHTML = acumulador;
 
 
 
@@ -85,10 +125,6 @@ document.write(acumulador);
 let listado = productosMayorPrecio.concat(productosMenorPrecio);
 //console.log(listado);
 
-// Quería hacerlo con un join pero se complicaba en un array de objetos y opte por el reduce #graciasStackoverflow
-//console.log(productos.reduce(function (a,b) {
-//    return (a.burger || a) + ", " + b.burger 
-//}));
 
 //SLICE para el paginado
 let paginadoUno = listado.slice(0,3);
@@ -100,14 +136,29 @@ let paginadoDos = listado.slice(3,5);
 
 
 
-//PUSH
-//console.log(productosMayorPrecio);
-//console.log(productosMenorPrecio);
+// PUSH
+// console.log(productosMayorPrecio);
+// console.log(productosMenorPrecio);
 
 
 
+// const cartStorage=localStorage.getItem('cart');
+// const cart=JSON.parse(cartStorage)??[];
 
+// const cart=cartStorage;
+// document.write('Tenes en el carrito:'+cart.length);
 
+// document.getElementById("producto").onclick=()=>{
+//     agregarAlCarrito({id:1,title:'Zapas',price:9000})
+// }
+
+// const agregarAlCarrito=(producto)=>{
+//     cart.push(producto.id)
+//     const cartJSON=JSON.stringify(cart)
+//     localStorage.setItem('cart',cart JSON);
+//     
+//     console.log(cart);
+// }
 
 
 
